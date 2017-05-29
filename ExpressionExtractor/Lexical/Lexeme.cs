195c2c085081e94d0e01b073ca800d934513360b
matejD04 <d.matej@outlook.com>
2017-05-29 14:43:13 +0200

    Added power operator type to LexemeType, added operator and associativity checking to Lexeme

--- ExpressionExtractor/Lexical/Lexeme.cs
+++ ExpressionExtractor/Lexical/Lexeme.cs
@@ -23,6 +23,7 @@ namespace OMathParser.Lexical
             OP_MINUS,
             OP_MUL,
             OP_DIV,
+            OP_POW,
             EQ_SIGN,
             ARGUMENT_SEPARATOR,
             OP_PLUS_UNARY,
@@ -44,36 +45,68 @@ namespace OMathParser.Lexical
             precedenceMap.Add(LexemeType.OP_MINUS, 2);
             precedenceMap.Add(LexemeType.OP_MUL, 3);
             precedenceMap.Add(LexemeType.OP_DIV, 3);
+            precedenceMap.Add(LexemeType.OP_POW, 4);
             precedenceMap.Add(LexemeType.EQ_SIGN, 1);
             precedenceMap.Add(LexemeType.ARGUMENT_SEPARATOR, -1);
-            precedenceMap.Add(LexemeType.OP_PLUS_UNARY, 4);
-            precedenceMap.Add(LexemeType.OP_MINUS_UNARY, 4);
+            precedenceMap.Add(LexemeType.OP_PLUS_UNARY, 5);
+            precedenceMap.Add(LexemeType.OP_MINUS_UNARY, 5);
         }
 
         private LexemeType type;
         private String value;
         private int precedence;
         private IToken parent;
+        private bool rightAssociative;
+        private bool isOperator;
 
         public Lexeme(LexemeType t, String value)
         {
             this.type = t;
             this.value = value;
             this.precedence = precedenceMap[t];
+            this.rightAssociative = false;
+            this.isOperator = false;
+
+            if (t == LexemeType.OP_POW || t == LexemeType.OP_PLUS_UNARY || t == LexemeType.OP_MINUS_UNARY)
+            {
+                this.rightAssociative = true;
+            }
+
+            if (t == LexemeType.EQ_SIGN ||
+                t == LexemeType.OP_DIV ||
+                t == LexemeType.OP_MINUS ||
+                t == LexemeType.OP_MINUS_UNARY ||
+                t == LexemeType.OP_MUL ||
+                t == LexemeType.OP_PLUS ||
+                t == LexemeType.OP_PLUS_UNARY ||
+                t == LexemeType.OP_POW)
+            {
+                this.isOperator = true;
+            }
         }
 
         public LexemeType Type { get => type; set => type = value; }
         public String Value { get => value; set => this.value = value; }
         public IToken Parent { get => null; set => this.parent = value; }
 
-        public bool isHigherPrecedence(Lexeme other)
+        public bool IsOperator()
+        {
+            return this.isOperator;
+        }
+         
+        public bool IsHigherPrecedence(Lexeme other)
         {
             return this.precedence > other.precedence;
         }
 
-        public bool isLowerPrecedence(Lexeme other)
+        public bool IsLowerPrecedence(Lexeme other)
         {
             return this.precedence < other.precedence;
+        }
+
+        public bool IsRightAssociative()
+        {
+            return this.rightAssociative;
         }
 
         public string simpleRepresentation()
