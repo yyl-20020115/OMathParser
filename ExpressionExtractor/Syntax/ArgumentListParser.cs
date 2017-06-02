263f8bb10892b84c15faa5f02e01a6635c794bc5
matejD04 <d.matej@outlook.com>
2017-06-02 21:55:24 +0200

    Added a specialization of the BaseOXMLParser, specialized to parse some forms of function argument lists

---
+++ ExpressionExtractor/Syntax/ArgumentListParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMathParser.Utils;
+
+namespace OMathParser.Syntax
+{
+    class ArgumentListParser : BaseOXMLParser
+    {
+        public ArgumentListParser(ParseProperties properties) : base(properties)
+        {
+        }
+    }
+}
