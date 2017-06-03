9dfbe5fbc14cf77174f23b69839f54f63f2d1912
matejD04 <d.matej@outlook.com>
2017-06-04 00:09:54 +0200

    Added EqualsNode syntax tree node

---
+++ ExpressionExtractor/Syntax/Nodes/EqualsNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OMathParser.Syntax.Nodes.Abstract;
+
+namespace OMathParser.Syntax.Nodes
+{
+    class EqualsNode : BinaryNode
+    {
+        public EqualsNode(SyntaxNode left, SyntaxNode right) 
+            : base(left, right)
+        {
+        }
+
+        public override double getValue()
+        {
+            return right.getValue();
+        }
+
+        public override string simpleRepresentation()
+        {
+            return String.Format("EqualsNode: {0} = {1}", left.simpleRepresentation(), right.simpleRepresentation());
+        }
+    }
+}
