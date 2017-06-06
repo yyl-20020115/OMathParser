21310113cef8eb4639de89124248a2b3495373e8
matejD04 <d.matej@outlook.com>
2017-06-06 16:33:48 +0200

    Created OMathPlotter project

---
+++ OMathPlotter/Forms/PlotForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace OMathPlotter.Forms
+{
+    partial class PlotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotViewPanel = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // plotViewPanel
+            // 
+            this.plotViewPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotViewPanel.Location = new System.Drawing.Point(12, 27);
+            this.plotViewPanel.Name = "plotViewPanel";
+            this.plotViewPanel.Size = new System.Drawing.Size(260, 222);
+            this.plotViewPanel.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // PlotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.plotViewPanel);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "PlotForm";
+            this.Text = "PlotForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plotViewPanel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+    }
+}
---
+++ OMathPlotter/Forms/PlotForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OxyPlot.WindowsForms;
+
+namespace OMathPlotter.Forms
+{
+    public partial class PlotForm : Form
+    {
+        public PlotForm()
+        {
+            InitializeComponent();
+            InitializePlotView();
+        }
+
+        private void InitializePlotView()
+        {
+
+        }
+    }
+}
