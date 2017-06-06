37a10c5ae84727c699e4c3f14d13eea9c20054dd
matejD04 <d.matej@outlook.com>
2017-06-06 16:34:55 +0200

    Added expression plotting utils

---
+++ OMathPlotter/Util/ExpressionUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OMathParser.Syntax;
+using OMathParser.Syntax.Nodes;
+using OMathParser.Syntax.Nodes.Abstract;
+
+namespace OMathPlotter.Util
+{
+    class ExpressionUtils
+    {
+        public static bool IsFunctionDefinition(SyntaxTree expression)
+        {
+            if (expression.RootNode is FunctionApplyNode)
+            {
+                FunctionApplyNode funcDefinition = expression.RootNode as FunctionApplyNode;
+                ArgumentListNode arguments = funcDefinition.Arguments;
+                foreach (SyntaxNode argument in arguments)
+                {
+                    if (!(argument is VariableIdentifierNode))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CanPlot2D(SyntaxTree expression)
+        {
+            return IsFunctionDefinition(expression) &&
+                (expression.RootNode as FunctionApplyNode).ArgumentsCount == 1;
+        }
+    }
+}
